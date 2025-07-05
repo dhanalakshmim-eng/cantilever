b06079dc84b6d60113050e0b0e20884bb1b07b49
dhanalakshmim-eng <dd406652dhana@gmail.com>
2025-07-05 21:51:36 +0530

    Create visualize.py

---
+++ WebScraping_Ecommerce/visualize.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+df = pd.read_excel("books_data.xlsx")
+rating_map = {'One': 1, 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5}
+df['Rating_Num'] = df['Rating'].map(rating_map)
+plt.figure(figsize=(8, 4))
+sns.histplot(df["Price"], kde=True)
+plt.title("Price Distribution")
+plt.xlabel("Price (£)")
+plt.ylabel("Count")
+plt.show()
+plt.figure(figsize=(8, 4))
+sns.boxplot(x="Rating", y="Price", data=df)
+plt.title("Price by Rating")
+plt.show()
